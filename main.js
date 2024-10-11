b4841189411f979f4d870ca2f507878484511322
Milan Patel <milanapatel23@gmail.com>
2024-10-11 01:14:45 -0400

    home route added for vercel to see it's actual working

--- main.js
+++ main.js
@@ -15,6 +15,11 @@ app.use(express.json()); // Parse incoming JSON
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/emp', employeeRoutes);
 
+// Home page route (this handles GET requests to "/")
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to My Website </h1><p>This is the home page for Vercel!</p>');
+});
+
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
